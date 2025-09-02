f351b986a091da03f7c9f9070bfa282b386c5524
colinyqt <colinyqt@utexas.edu>
2025-09-02 13:38:42 -0500

    new file

---
+++ CountEight.java
@@ -0,0 +1,15 @@
+public class CountEight {
+    public static void main(String[] args) {
+        int count = 0;
+        for(int i = 0; i <= 100; i++) {
+            String numStr = Integer.toString(i);
+            for(char c : numStr.toCharArray()) {
+                if(c == '8') {
+                    count++;
+                }
+            }
+        }
+        System.out.println("The digit eight appears " + count + " times.");
+    }
+}
+
